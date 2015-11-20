957d46f20f0eaefb8f5f61c0bf82fe4336029706
Kaijie Zheng <kaijiejz50@gmail.com>
2015-11-19 19:38:43 -0800

    (refactor) Replace MongoDB with PostreSQL with Sequelize

--- server/db/database.js
+++ server/db/database.js
@@ -1,18 +1,65 @@
-var mongoose = require('mongoose');
-var autoIncrement = require('mongoose-auto-increment');
+var Sequelize = require("sequelize");
 
-// mongoURI = 'mongodb://localhost/lancealot';
-mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/lancealot';
-mongoose.connect(mongoURI);
+// need to set this up for deployment
+// var orm = new Sequelize("lancealot", "root", "pass");
+var orm = new Sequelize('postgres://root:pass@localhost.com:5432/lancealot');
 
-// Run in seperate terminal window using 'mongod'
-var db = mongoose.connection;
-
-autoIncrement.initialize(db);
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
- console.log('Mongodb connection open');
+var Freelancer = orm.define('Freelancer', {
+  email: Sequelize.STRING,
+  password: Sequelize.STRING,
+  name: Sequelize.STRING
 });
 
-module.exports = db;
+var Client = orm.define('Client', {
+  name: Sequelize.STRING,
+  address: Sequelize.STRING,
+  phone: Sequelize.STRING
+});
+
+var Job = orm.define('Job', {
+  start: Sequelize.DATE,
+  end: Sequelize.DATE,
+  rate: Sequelize.INTEGER,
+  description: Sequelize.STRING(1234)
+});
+
+var Time = orm.define('Time', {
+  start: Sequelize.DATE,
+  end: Sequelize.DATE
+});
+
+Freelancer.hasMany(Job);
+Client.hasMany(Job);
+Job.hasMany(Time);
+
+Freelancer.sync({ force: true });
+Client.sync({ force: true });
+Job.sync({ force: true });
+Time.sync({ force: true });
+
+exports.Freelancer = Freelancer;
+exports.Client = Client;
+exports.Job = Job;
+exports.Time = Time;
+
+// // Use cases
+// var newFL = Freelancer.build({
+//   email: "aol@aol.com",
+//   name: "Fossil",
+//   password: "password"
+// });
+
+// newFL.save().success(function() {
+//   // callback
+
+// });
+
+// Freelancer.findAll({ where: { name: "Fossil" } }).success(function(flers) {
+//   // callback
+// });
+
+// Freelancer.findOrCreate({
+//   where: { name: "Elon", email: "elon@tesla.com", password: "complicated" }
+// }).then(function(fler) {
+//   // callback
+// });
