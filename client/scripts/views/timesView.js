3b1bc93d5378362173a2cf5bbb2f5cd13549a61a
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2015-11-20 16:28:45 -0800

    (feat) setting up intialization and rendering of bar graph

--- client/scripts/views/timesView.js
+++ client/scripts/views/timesView.js
@@ -8,11 +8,11 @@ Lancealot.TimesView = Backbone.View.extend({
   chartOptions: {
     width: 400,
     height: 450
-  }
+  },
   initialize: function() {
     this.collection.on('change', this.render, this);
     
-    //initialize graph
+    //initialize SVG graph
     chart = d3.selectAll($(this.el))
               .append("svg")
               .attr("class", "chart")
@@ -20,21 +20,28 @@ Lancealot.TimesView = Backbone.View.extend({
               .attr("height", this.chartOptions.height)
               .append("g")
               .attr("transform", "translate(10,15)");
-
+              //render?
     this.collection.fetch();
   },
   render: function() {
+    var x = d3.scale.ordinal()
+        .domain(/* 7? */)
+        .rangeRoundBands([0, this.chartOptions.width], .1);
+
+    var y = d3.scale.linear()  
+            .domain([0, 7])
+            .range([0, this.chartOptions.height]);
     //render time data onto graph
     this.chart.selectAll('rect')
-      .data('../../../seeds/times.json'/* this.collection? */)
+      .data("../../../seeds/times.json"/* this.collection? */)
       .enter()
-      .append('rect')
-      .attr('class', 'bar')
-      .attr('x', )
-      .attr('y', )
-      .attr('width', (this.chartOptions.width / 7) - 1)
-      .attr('height', 150 /*total time*/)
-      .attr('fill', 'teal')
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", )
+      .attr("y", function(d, i) { return i * 20 })
+      .attr("width", (this.chartOptions.width / 7) - 1)
+      .attr("height", 150 /*total time function(d) { return d.total }*/)
+      .attr("fill", "teal")
 
   }
 });
