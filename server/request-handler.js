da4192938e45b86ddffb2461ad921877b03ca538
Kaijie Zheng <kaijiejz50@gmail.com>
2015-11-21 15:24:38 -0800

    (refactor) Change all request handler methods to utilize bookshelf

--- server/request-handler.js
+++ server/request-handler.js
@@ -2,19 +2,29 @@ var bcrypt = require('bcrypt-nodejs');
 var util = require('./utility');
 var request = require('request');
 
-var db = require('./db/database');
-var Freelancer = db.Freelancer;
-var Client = db.Client;
-var Job = db.Job;
-var Time = db.Time;
+// var db = require('./db/database');
+// var Freelancer = db.Freelancer;
+// var Client = db.Client;
+// var Job = db.Job;
+// var Time = db.Time;
+var Freelancer = require('./db/models/freelancer');
+var Client = require('./db/models/client');
+var Job = require('./db/models/job');
+// var Time = require('./models/time');
 
 /*
 fetchClients is called when /clients path receives get request
 Finds all clients in the database and responds with result of query
 */
 exports.fetchClients = function(req, res) {
-  Client.findAll().then(function(clients) {
-    res.send(200, clients);
+  new Client()
+  .fetchAll()
+  .then(function(clients) {
+    console.log('Fetched all clients successfully');
+    res.status(200).send(clients);
+  })
+  .catch(function(err) {
+    console.log('Error in retrieving clients:', err.message);
   });
 };
 
@@ -22,15 +32,32 @@ exports.fetchClients = function(req, res) {
 Builds new Client document with request properties and saves it to the db
 */
 exports.addClient = function (req, res) {
-  var newClient = new Client({
-    name: req.body.name,
-    address: req.body.address,
-    phone: req.body.phone
-  });
-
-  newClient.save().then(function (newClient) {
-    console.log('added new client: ', newClient);
-    res.send(200, newClient);
+  new Client({ name: req.body.name })
+  .fetch()
+  .then(function(client) {
+    if (client) {
+      console.log('Client is already in our database');
+      res.redirect('/addclient');
+    } else {
+      new Client({
+        name: req.body.name,
+        address: req.body.address,
+        phone: req.body.phone
+      })
+      .save()
+      .then(function(client) {
+        console.log('Saved client successfully');
+        res.status(200).send(client);
+      })
+      .catch(function(err) {
+        console.log('Error while saving new client:', err.message);
+        res.redirect('/addclient');
+      });
+    }
+  })
+  .catch(function(err) {
+    console.log('Error in trying to see if client already exists in database:', err.message);
+    res.redirect('/addclient');
   });
 };
 
@@ -40,8 +67,20 @@ Finds all jobs in the database, replaces client_id with an object that include c
 Responds with result of query
 */
 exports.fetchJobs = function (req, res) {
-  Job.findAll().then(function(jobs) {
-    res.send(jobs)
+  new Job()
+  .fetchAll({
+    withRelated: [
+      'client'
+    ]
+  })
+  .then(function(jobs) {
+    console.log('Fetched all jobs successfully');
+    console.log(jobs);
+    res.status(200).send(jobs);
+  })
+  .catch(function(err) {
+    console.log('blahblah');
+    console.log('Error in retrieving jobs:', err.message);
   });
 };
 
@@ -49,12 +88,58 @@ exports.fetchJobs = function (req, res) {
 Builds new Job document with request properties and saves it to the db
 */
 exports.addJob = function (req, res) {
-  //call createJobDoc first to find client id to use to create job document
-  console.log('req body in addJob: ', req.body);
-  //check if id already exists
-
-  Job.findById(req.body._id).then(function(job) {
-    util.createOrUpdateJob(req, res, job);
+  new Client({
+    name: req.body.client
+  })
+  .fetch()
+  .then(function(client) {
+    if (client) {
+      new Job({
+        description: req.body.description,
+        rate: req.body.rate,
+        start: new Date(req.body.start),
+        end: new Date(req.body.end),
+        client_id: client.id,
+        freelancer_id: req.session.user.id
+      })
+      .fetch()
+      .then(function(job) {
+        if (!job) {
+          new Job({
+            description: req.body.description,
+            rate: req.body.rate,
+            start: new Date(req.body.start),
+            end: new Date(req.body.end),
+            status: false,
+            client_id: client.id,
+            freelancer_id: req.session.user.id
+          })
+          .save()
+          .then(function(job) {
+            console.log('Saved job successfully');
+            res.status(200).send(job);
+          })
+          .catch(function(err) {
+            console.log('Error in saving job', err.message);
+          });
+        } else {
+          console.log('Job already exists for this client, updating status instead');
+          job.status = req.body.status;
+          res.redirect('/');
+        }
+      })
+      .catch(function(err) {
+        console.log('Error in detecting if job is already in database:', err.message);
+        res.redirect('/addjob');
+      })
+    } else {
+      console.log('Could not find client for this job');
+      res.redirect('/addjob');
+    }
+  })
+  .catch(function(err) {
+    console.log('Error in fetching client id to add job', err.message);
+    res.redirect('/addjob');
   });
 };
 
@@ -67,16 +152,27 @@ Receives all toggl info for later use, and creates user session
 exports.loginUser = function (req, res) {
   var email = req.body.email;
   var password = req.body.password;
-
-  User.findOne({ email: email })
-    .exec(function (err, user) {
-      if (user === null) {
-        res.redirect('/signup');
+  console.log(password);
+  new Freelancer({ email: email })
+  .fetch()
+  .then(function(freelancer) {
+    if (freelancer) {
+      // change to compare bcrypt
+      if (freelancer.get('password') === password) {
+        console.log('Passwords match');
+        util.createSession(req, res, freelancer);
       } else {
-        // need to bcrypt password and check
-        // create session if password matches
-        util.createSession(req, res, user);
+        console.log('Wrong password');
+        res.redirect('/login');
       }
+    } else {
+      console.log('Could not find account');
+      res.redirect('/signup');
+    }
+  })
+  .catch(function(err) {
+    console.log('Error in retrieving account from database:', err.message);
+    res.redirect('/login');
   });
 };
 
@@ -88,26 +184,36 @@ Api token is taken from toggl resp to build new User document, then saves it
 If save is successful, new user session is created
 If user exists, redirect to /login
 */
-exports.signupUser = function (req, res) {
+exports.signupUser = function(req, res) {
   var email = req.body.email;
   var password = req.body.password;
 
-  User.findOne({ email: email })
-    .exec(function (err, user) {
-      if (user === null) {
-
-        // bcrypt password
-        var newUser = new User({
-          email: email,
-          password: password
-        });
-
-        newUser.save().success(function () {
-          util.createSession(req, res, newUser);
-        });
-      } else {
-        console.log('Account already exists');
-        res.redirect('/login');
-      }
-    });
+  new Freelancer({
+    email: email,
+  })
+  .fetch()
+  .then(function(freelancer) {
+    if (!freelancer) {
+      new Freelancer({
+        email: email,
+        password: password
+      })
+      .save()
+      .then(function(freelancer) {
+        console.log('Saved freelancer successfully');
+        util.createSession(req, res, freelancer);
+      })
+      .catch(function(err) {
+        console.log('Error in saving freelancer', err.message);
+        res.redirect('/signup');
+      });
+    } else {
+      console.log('Freelancer already exists');
+      res.redirect('/signup');
+    }
+  })
+  .catch(function(err) {
+    console.log('Error in seeing if freelancer is in database:', err.message);
+    res.redirect('/signup');
+  });
 };
